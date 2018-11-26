c894ef76d14457e4c929e63c27d4a23c24ef943a
Anubhav Singh <anubhavsingh@Anubhavs-MacBook-Air.local>
2018-11-26 11:37:14 +0530

    add models without migration

--- wyngcommerce/apps/common/admin.py
+++ wyngcommerce/apps/common/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.common.models import Tenant
 
 # Register your models here.
+
+admin.site.register(Tenant)
--- wyngcommerce/apps/common/models.py
+++ wyngcommerce/apps/common/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.IntegerField(default=0)
+    updated_by = models.IntegerField(default=0)
+    is_deleted = models.BooleanField(default=False)
+
+
+class Tenant(BaseModel):
+    name = models.CharField(max_length=100)
+    subdomain_prefix = models.CharField(max_length=100, unique=True)
+
+
+class TenantAwareModel(BaseModel):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
